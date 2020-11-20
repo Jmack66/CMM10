063801b3335ba2c1c0f86b9affc46befabc262e2
mack.jonah@gmail.com <mack.jonah@gmail.com>
2020-11-20 02:19:01 -0800

    msic

--- CMM10_test.py
+++ CMM10_test.py
@@ -3,5 +3,5 @@ import CoeffecientData as data
 import AircraftDynamics as ps
 import GUI as gui
 if __name__ == "__main__":
-	coeffs,covar = data.set_coeffecients() #testing 
+	# coeffs,covar = data.set_coeffecients() #testing 
 	#gui.app.mainloop()
