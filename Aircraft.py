41dd883261b2fb528173f9199eb0589dddd31081
mack.jonah@gmail.com <mack.jonah@gmail.com>
2020-11-25 13:37:41 -0800

    currently working Aircraft for initial trims

--- Aircraft.py
+++ Aircraft.py
@@ -8,7 +8,7 @@ import CoeffecientData as cd
 
 #EVERYTHING IS IN RADIANS GOOOOOOOOOOOOOOOD
 
-testing = {"Velocity" : 100, "Flight Path" : 0.05, "Altitude" : -1000, "Time Step" : 0.01}
+testing = {"Velocity" : 80, "Flight Path" : 0.0, "Altitude" : -1000, "Time Step" : 0.01}
 
 class Aircraft():
     def __init__(self, initials):
@@ -19,22 +19,18 @@ class Aircraft():
         self.mass = veh.acMass # this line isnt really needed but wanted to be able to access mass with .mass
         self.weight = veh.acMass * env.gravity
         self.coeff,self.null = cd.set_coeffecients()
-        self.delta_el = 0
-    def trim(self,alpha_x):
-        self.delta_el = -(self.coeff.get("CM_0") + self.coeff.get("CM_alpha")*alpha_x)/self.coeff.get("CM_delta")
-        self.delta_el = np.deg2rad(self.delta_el)
-        print(self.delta_el)
+    def trim(self,a):#alpha in is radians
+        self.delta_el = -(self.coeff.get("CM_0") + self.coeff.get("CM_alpha")*np.degrees(a))/self.coeff.get("CM_delta")
         return self.delta_el
-    def get_CL(self,alpha_x):
-        alpha_x = np.degrees(alpha_x)
-        return self.coeff.get("CL_0") + self.coeff.get("CL_alpha")*alpha_x + self.coeff.get("CL_delta")*self.delta_el #for now using the class deltael variable as opposed to calling the function-- understanding is that del el doesnt change
-    def get_CD(self,alpha_x):
-        return self.coeff.get("CD_0") + self.coeff.get("CD_K")*self.get_CL(alpha_x)**2
-    def get_CM(self,alpha):
-        return self.coeff.get("CM_0")+(self.coeff.get("CM_alpha")*self.alpha)+(self.coeff.get("CM_delta")*self.delta_el)
-    def set_theta(self,alpha_x):
-        self.theta = alpha_x + self.gamma
-        return self.theta
+    def get_CL(self,a):#alpha in is radians
+        return self.coeff.get("CL_0") + self.coeff.get("CL_alpha")*np.degrees(a) + self.coeff.get("CL_delta")*self.trim(a) #for now using the class deltael variable as opposed to calling the function-- understanding is that del el doesnt change
+    def get_CD(self,a):#alpha in radians
+        return self.coeff.get("CD_0") + self.coeff.get("CD_K")*self.get_CL(a)**2
+    def get_CM(self,a):
+        return self.coeff.get("CM_0")+(self.coeff.get("CM_alpha")*np.degrees(a))+(self.coeff.get("CM_delta")*self.trim(a))
+    def set_theta(self,a): #alpha in rads
+        self.theta = a + self.gamma
+        return self.theta #theta in rads
     def get_drag(self,a):
         self.Drag = 0.5 * env.air_density * self.V**2 * veh.Sref * self.get_CD(a)
         return self.Drag
@@ -42,12 +38,12 @@ class Aircraft():
         self.Lift = 0.5 * env.air_density * self.V**2 * veh.Sref * self.get_CL(a)
         return self.Lift
     def get_thrust(self,a):
-        self.Thrust = self.Drag * np.cos(a) + self.weight * np.sin(self.set_theta(a)) - self.Lift * np.sin(a)
+        self.Thrust = self.Drag * np.cos(math.radians(a)) + self.weight * np.sin(self.set_theta(math.radians(a))) - self.Lift * np.sin(math.radians(a))
         return self.Thrust
     def get_moment(self,a):
         self.Moment = (env.air_density*veh.Sref*veh.cbar*self.get_CM(a)*self.V**2)/2
-    def find_alpha(self,a):
-        return (0.5 * env.air_density * self.V**2 * veh.Sref) * (self.get_CL(a) * np.cos(a) + self.get_CD(a) * np.sin(a)) - self.weight * np.cos(self.set_theta(a))
+    def find_alpha(self,alpha_rad):
+        return (0.5 * env.air_density * self.V**2 * veh.Sref) * (self.get_CL(alpha_rad) * np.cos(alpha_rad) + self.get_CD(alpha_rad) * np.sin(alpha_rad)) - self.weight * np.cos(self.set_theta(alpha_rad))
     def get_alpha(self):
         self.alpha = optimize.fsolve(self.find_alpha,0)
         return self.alpha
@@ -68,7 +64,7 @@ class Aircraft():
         self.get_alpha()
         self.trim(self.alpha)
         self.get_forces(self.alpha)
-        print(self.alpha)
+        print(self.alpha,np.radians(self.delta_el))
 
 
 
